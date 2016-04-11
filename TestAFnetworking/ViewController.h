e1e450053a127f19105b508ca632cfcf0cc9ca98
namld01810 <namld01810@gmail.com>
2016-04-11 16:46:16 +0700

    Initial Commit

---
+++ TestAFnetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAFnetworking
+//
+//  Created by MACBOOK AIR on 4/11/16.
+//  Copyright © 2016 NamLD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAFnetworking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestAFnetworking
+//
+//  Created by MACBOOK AIR on 4/11/16.
+//  Copyright © 2016 NamLD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
