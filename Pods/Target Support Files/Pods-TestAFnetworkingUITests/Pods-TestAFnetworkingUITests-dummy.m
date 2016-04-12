ae13d524bb4c41173d04d617a2e7f2a9336f8695
namld01810 <namld01810@gmail.com>
2016-04-12 10:21:47 +0700

    first upload

---
+++ Pods/Target Support Files/Pods-TestAFnetworkingUITests/Pods-TestAFnetworkingUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestAFnetworkingUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestAFnetworkingUITests
+@end
--- TestAFnetworking/ViewController.m
+++ TestAFnetworking/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "AFURLSessionManager.h"
+
 
 @interface ViewController ()
 
@@ -17,6 +19,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString:@"http://game.sabi.vn/api/games/getDataHome.aspx?zip=1&token=edugame@idtvn&c=10"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+        } else {
+            //NSLog(@"response : %@\n responseObject : %@", response, responseObject);
+            NSLog(@" game : %@", [[[[responseObject valueForKey:@"data"] valueForKey:@"game"] objectAtIndex:0] valueForKey:@"content"]);
+        }
+    }];
+    [dataTask resume];
 }
 
 - (void)didReceiveMemoryWarning {
